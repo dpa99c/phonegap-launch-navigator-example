1aeab4edfd0f5065785125e525a0cd455f66df5c
Dave Alden <dpa99c@gmail.com>
2014-11-07 13:56:57 +0000

    Added iOS example

---
+++ LaunchNavigatorExample/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.device": "0.2.12",
+    "uk.co.workingedge.phonegap.plugin.LaunchNavigator": "2.0.0"
+}
+// BOTTOM OF METADATA
+});
