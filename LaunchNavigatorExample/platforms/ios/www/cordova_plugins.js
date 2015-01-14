7840ac6f828001e9576c6e2f58846ad3102af951
Dave Alden <dpa99c@gmail.com>
2015-01-14 21:34:10 +0000

    Added updated plugin to iOS

--- LaunchNavigatorExample/platforms/ios/www/cordova_plugins.js
+++ LaunchNavigatorExample/platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
+    {
+        "file": "plugins/uk.co.workingedge.phonegap.plugin.LaunchNavigator/www/ios/launchnavigator.js",
+        "id": "uk.co.workingedge.phonegap.plugin.LaunchNavigator.LaunchNavigator",
+        "clobbers": [
+            "launchnavigator"
+        ]
+    }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "uk.co.workingedge.phonegap.plugin.LaunchNavigator": "2.0.0"
 }
 // BOTTOM OF METADATA
 });
