ba759984c3799d1e7422ca7e22a5e8651898e23f
Dave Alden <dpa99c@gmail.com>
2016-09-26 12:44:12 +0200

    Add example usage of callback to receive the app selected in native picker

--- AdvancedExample/www/index.js
+++ AdvancedExample/www/index.js
@@ -98,6 +98,11 @@ function navigate(e){
         transportMode: values["transport-mode"],
         launchMode: values["launch-mode"],
         extras: parseExtras(values["extras"]),
+        appSelectionDialogHeader: "Custom header",
+        appSelectionCancelButton: "Custom cancel text",
+        appSelectionCallback: function(app){
+            console.info("User selectedapp app: "+app);
+        },
         enableDebug: true
     });
     return false;
